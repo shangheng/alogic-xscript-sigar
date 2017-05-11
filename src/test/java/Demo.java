49c1c964a55ce2bcae46a5d80f5dbaeab593a6cd
shangheng <18917372163@189.cn>
2017-05-11 14:29:51 +0800

    修改引用

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -4,8 +4,6 @@ import java.util.Map;
 import com.alogic.xscript.ExecuteWatcher;
 import com.alogic.xscript.LogicletContext;
 import com.alogic.xscript.Script;
-import com.alogic.xscript.doc.XsObject;
-import com.alogic.xscript.doc.json.JsonObject;
 import com.anysoft.util.CommandLine;
 import com.anysoft.util.Properties;
 import com.anysoft.util.Settings;
@@ -16,19 +14,18 @@ public class Demo {
 
 	public static void run(String src, Properties p) {
 		Script script = Script.create(src, p);
-		if (script == null) {
+		if(script == null) {
 			System.out.println("Fail to compile the script");
 			return;
 		}
 		long start = System.currentTimeMillis();
 		Map<String, Object> root = new HashMap<String, Object>();
-		XsObject doc = new JsonObject("root",root);
 		LogicletContext ctx = new LogicletContext(p);
-		script.execute(doc, doc, ctx, new ExecuteWatcher.Quiet());
-
+		script.execute(root, root, ctx, new ExecuteWatcher.Quiet());
+		
 		System.out.println("Script:" + src);
 		System.out.println("Duration:" + (System.currentTimeMillis() - start) + "ms");
-
+		
 		JsonProvider provider = JsonProviderFactory.createProvider();
 		System.out.println("#########################################################");
 		System.out.println(provider.toJson(root));
@@ -40,13 +37,13 @@ public class Demo {
 		settings.addSettings(new CommandLine(args));		
 		settings.addSettings("java:///conf/settings.xml#Demo", null, Settings.getResourceFactory());
 		run("java:///xscript/CPU.xml#Demo",settings);
-		run("java:///xscript/CPUInfo.xml#Demo",settings);
-		run("java:///xscript/OS.xml#Demo",settings);
-		run("java:///xscript/FileSystem.xml#Demo",settings);
-		run("java:///xscript/Info.xml#Demo",settings);
-		run("java:///xscript/Mem.xml#Demo",settings);
-		run("java:///xscript/NetInfo.xml#Demo",settings);
-		run("java:///xscript/NetStat.xml#Demo",settings);
-		run("java:///xscript/Process.xml#Demo",settings);
+//		run("java:///xscript/CPUInfo.xml#Demo",settings);
+//		run("java:///xscript/OS.xml#Demo",settings);
+//		run("java:///xscript/FileSystem.xml#Demo",settings);
+//		run("java:///xscript/Info.xml#Demo",settings);
+//		run("java:///xscript/Mem.xml#Demo",settings);
+//		run("java:///xscript/NetInfo.xml#Demo",settings);
+//		run("java:///xscript/NetStat.xml#Demo",settings);
+//		run("java:///xscript/Process.xml#Demo",settings);
 	}
 }
