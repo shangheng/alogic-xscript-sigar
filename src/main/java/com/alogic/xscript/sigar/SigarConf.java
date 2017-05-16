c013c4ee34986e7196dfc89a039c82ef776a207b
shangheng <18917372163@189.cn>
2017-05-16 14:14:18 +0800

    解决由于为了支持xml报文引起的插件bug

--- src/main/java/com/alogic/xscript/sigar/SigarConf.java
+++ src/main/java/com/alogic/xscript/sigar/SigarConf.java
@@ -1,11 +1,9 @@
 package com.alogic.xscript.sigar;
 
-import java.util.Map;
-
-
 import com.alogic.xscript.ExecuteWatcher;
 import com.alogic.xscript.Logiclet;
 import com.alogic.xscript.LogicletContext;
+import com.alogic.xscript.doc.XsObject;
 import com.alogic.xscript.plugins.Segment;
 import com.alogic.xscript.sigar.util.HostTools;
 
@@ -27,8 +25,8 @@ public class SigarConf extends Segment{
 	}
 	
 	@Override
-	protected void onExecute(Map<String, Object> root, 
-			Map<String, Object> current, LogicletContext ctx, ExecuteWatcher watcher) {
+	protected void onExecute(XsObject root, 
+			XsObject current, LogicletContext ctx, ExecuteWatcher watcher) {
 		HostTools hostTool = new HostTools();
 		try {
 			ctx.setObject(cid, hostTool);
